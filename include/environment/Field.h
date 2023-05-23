782face0036dbe07c49ce37eecf97788a7f7e234
IbrahimSquared <ibrahim.ibrahim@kuleuven.be>
2023-05-23 17:05:51 +0200

    change back smart pointer

--- include/environment/Field.h
+++ include/environment/Field.h
@@ -18,13 +18,8 @@ class Field {
     if constexpr (unique == 0)
       data_ = std::make_unique<T[]>(size_);
     else
-      // data_ = std::make_shared<T[]>(size_); C++20
-      // data_ = std::shared_ptr<T[]>(new T[size_], std::default_delete<T[]>()); C++ 11
-      #if __cplusplus > 201103L // Check if C++20 or later
-        data_ = std::make_shared<T[]>(size_);
-      #else // C++11 or earlier
-        data_ = std::shared_ptr<T[]>(new T[size_], std::default_delete<T[]>());
-      #endif
+      // data_ = std::make_shared<T[]>(size_);
+      data_ = std::shared_ptr<T[]>(new T[size_], std::default_delete<T[]>());
     fill(default_value);
   }
 
