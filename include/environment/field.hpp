0f9b9de49af22f23860869e993074dc18aa4b13f
IbrahimSquared <ibrahim.ibrahim@kuleuven.be>
2024-02-11 15:22:18 +0100

    refactor field

--- include/environment/field.hpp
+++ include/environment/field.hpp
@@ -11,7 +11,7 @@ public:
   using size_t = std::size_t;
   Field() : nx_(0), ny_(0), size_(0), data_(nullptr) {}
 
-  explicit Field(size_t nx, size_t ny, T default_value)
+  explicit Field(const size_t nx, const size_t ny, const T default_value)
       : nx_(nx), ny_(ny), size_(nx * ny) {
     data_ = std::make_unique<T[]>(size_);
     for (size_t i = 0; i < size_; ++i) {
